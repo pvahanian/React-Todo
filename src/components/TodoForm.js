53880ccbcff7d21af8902df016279457befc4457
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-09-28 19:51:35 -0700

    Complete mvp

---
+++ src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+// import Todo from './Todo';
+// import TodoList from './TodoList';
+
+class ToDoForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            toDo: ''
+        }
+    }
+
+    handleChanges = e => {
+        this.setState ({ [e.target.name]: e.target.value })
+    }
+
+    submitToDo = e => {
+        e.preventDefault()
+        this.setState({ toDo:'' })
+        this.props.addToDo(e, this.state.toDo)
+    }
+
+    render() {
+        console.log('rendering form')
+        return (
+            <form onSubmit={this.submitToDo}>
+                <input
+                    type = 'text'
+                    placeholder = '...To Do'
+                    value = {this.state.toDo}
+                    name = 'toDo'
+                    onChange = {this.handleChanges}
+                />
+                <button>Add Task!</button>
+            </form>
+
+        )
+    }
+}
+
+export default ToDoForm
